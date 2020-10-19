ea5346d5e665314edacba10b10b17a29e7938435
Andika Nugraha <m.andika.nugraha@gmail.com>
2020-10-19 15:45:34 +0700

    Update using-tornado.py

--- using-tornado.py
+++ using-tornado.py
@@ -1,16 +1,32 @@
 import tornado.ioloop
 import tornado.web
+import datetime
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.write("Hello world")
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
     ])
+    
+def startTornado(*args, **kwargs):
+    app = make_app()
+    server = app.listen(8888)
+    print("Running Hello World on localhost:8888")
+    return server
+
+def stopTornado():
+    tornado.ioloop.IOLoop.instance().stop()
 
 if __name__ == "__main__":
-    app = make_app()
-    app.listen(8888)
+    server = startTornado()
+    print("Hello World")
+    print("The server will exit after 30 seconds")
+    tornado.ioloop.IOLoop.current().add_timeout(
+        datetime.timedelta(seconds=30),
+        stopTornado)
     tornado.ioloop.IOLoop.current().start()
+    print("Server exit")
+    server.stop()
