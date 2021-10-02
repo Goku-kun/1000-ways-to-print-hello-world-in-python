19b2e1cd7be7a4625e01d2c626781501bcbb5673
AdityaWadkar <67093170+AdityaWadkar@users.noreply.github.com>
2021-10-02 14:50:14 +0530

    few more ways to print hello world
    printed hello world using Fstring module and using while loop

--- hello_world_print.py
+++ hello_world_print.py
@@ -1,3 +1,15 @@
 # A program to print hello world using the print function
 
 print('Hello, World!')
+
+#program to print hello world using fstring
+a='world'
+print(f"hello {a}")
+
+#print hello world multiple times using while loop
+
+i=1
+print("\nprinting hello world multiple times\n")
+while i<=10:
+    print("hello world")
+    i+=1
