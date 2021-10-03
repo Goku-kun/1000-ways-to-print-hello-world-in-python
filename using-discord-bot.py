be91f9d96cfede6548971414ca39abca6255fe88
RKNpy <80162047+RKNpy@users.noreply.github.com>
2021-10-03 12:58:45 +0530

    Update using-discord-bot.py

--- using-discord-bot.py
+++ using-discord-bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-bot = discord.Bot(command_prefix='!', help_command=None)
+bot = commands.Bot(command_prefix='!', help_command=None)
 
 @bot.event
 async def on_ready():
