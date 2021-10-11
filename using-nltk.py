3572848833a3c9d5950706e63beef4f4fdf0c3bf
Dharmarajsinh Jethva <jethvadharmarajsinh@gmail.com>
2021-10-11 08:10:25 -0700

    Update using-nltk.py

--- using-nltk.py
+++ using-nltk.py
@@ -1,6 +1,7 @@
 # Printing Hello World using nltk
 import nltk
 
+# nltk.download('punkt')  # download for first run
 sentence = "This is a sentence containing Hello World"
 target_words = nltk.word_tokenize(sentence)[-2:]
 
