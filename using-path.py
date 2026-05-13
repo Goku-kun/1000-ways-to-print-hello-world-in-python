0a4cfdf5e1007c748d89a07aaa2a19f9eeba3d60
Stacey Melnick <stacey.melnick.np@gmail.com>
2026-05-13 09:08:03 -0700

    adding my own way of printing hello

---
+++ using-path.py
@@ -0,0 +1,20 @@
+
+path=[
+    72,
+    29,
+    7,
+    0,
+    3,
+    -79,
+    55,
+    24,
+    3,
+    -6,
+    -8,
+    -67
+]
+
+position=0
+for i in path:
+    position+=i
+    print(chr(position),end='')
