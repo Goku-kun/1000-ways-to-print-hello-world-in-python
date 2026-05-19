fc9e2e7a82568a1f0818534beefca013ec5c300e
Stacey Melnick <stacey.melnick.np@gmail.com>
2026-05-19 07:32:01 -0700

    made corrections

--- using-path.py
+++ using-path.py
@@ -1,20 +1,21 @@
 
-path=[
-    72,
-    29,
-    7,
-    0,
+path = [
+    72, 
+    29, 
+    7, 
+    0, 
     3,
-    -79,
-    55,
-    24,
-    3,
-    -6,
-    -8,
-    -67
+    -67, 
+    -12, 
+    55, 
+    24, 
+    3, 
+    -6, 
+    -8, 
+    -67 
 ]
 
 position=0
 for i in path:
     position+=i
-    print(chr(position),end='')
+    print(chr(position), end='')
